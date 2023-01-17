50458041bcf96276f59ae6149f0bbbaabf542033
Krzysztof Bąk <github@itachi.pl>
2023-01-17 11:51:32 +0100

    [GH-0] Add CMakeLists.txt and toolchain.cmake.

---
+++ nrf-template/toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER "C:/Program Files (x86)/GNU Arm Embedded Toolchain/9 2020-q2-update/bin/arm-none-eabi-gcc.exe")
+set(CMAKE_ASM_COMPILER "C:/Program Files (x86)/GNU Arm Embedded Toolchain/9 2020-q2-update/bin/arm-none-eabi-gcc.exe")
