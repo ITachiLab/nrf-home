3c0aeb148de46cf06f4da00a03a926338b9a5fc8
Krzysztof Bąk <github@itachi.pl>
2023-01-21 13:46:14 +0100

    [GH-0] Improve building system by using config.json. Add "flash_all" target for flashing SoftDevice along with the application.

--- nrf-template/toolchain.cmake
+++ nrf-template/toolchain.cmake
@@ -1,5 +1,14 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER "C:/Program Files (x86)/GNU Arm Embedded Toolchain/9 2020-q2-update/bin/arm-none-eabi-gcc.exe")
-set(CMAKE_ASM_COMPILER "C:/Program Files (x86)/GNU Arm Embedded Toolchain/9 2020-q2-update/bin/arm-none-eabi-gcc.exe")
+if(NOT DEFINED ENV{GCC_PATH} AND NOT DEFINED ENV{SDK_ROOT})
+    file(READ "config.json" config)
+    string(JSON GCC_PATH GET ${config} gccPath)
+    string(JSON SDK_ROOT GET ${config} sdkRoot)
+
+    set(ENV{GCC_PATH} ${GCC_PATH})
+    set(ENV{SDK_ROOT} ${SDK_ROOT})
+endif()
+
+set(CMAKE_C_COMPILER "${GCC_PATH}")
+set(CMAKE_ASM_COMPILER "${GCC_PATH}")
